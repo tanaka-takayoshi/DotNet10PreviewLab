aa8c7d51162178db6af5c112c2527848d3634bbe
tanaka_733 <tanaka-takayoshi@users.noreply.github.com>
2025-03-29 13:13:06 +0900

    プロジェクト ファイルを追加します。

---
+++ DotNet10PreviewLab/Etc.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotNet10PreviewLab
+{
+    internal class Etc
+    {
+        delegate int DR(string s);
+        delegate void DO(string s, out int i);
+
+        public static void Execute()
+        {
+            //unbound generic types in nameof
+            var t = typeof(List<>);
+            var n = nameof(List<>);
+
+            DR w1 = (s) => s.Length;
+            DO w2 = (s, out i) => i = s.Length;
+
+            var f = new Fields2 { FirstName = "  First  ", FieldProperty = "  FieldProperty  ", AutoProperty = "  AutoProperty  " };
+            Console.WriteLine($"{f.field}");
+
+        }
+    }
+
+    class Fields
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get => field; set => field = value.Trim(); }
+    }
+
+    class Fields2
+    {
+        public string? field;
+        public string? FirstName { get; set; }
+        public string? FieldProperty { get => @field; set => @field = value.Trim(); }
+        public string? AutoProperty { get => field; set => field = value.Trim(); }
+    }
+}
---
+++ DotNet10PreviewLab/OrderedDictionaryTryAdd.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Reflection;
+using System.Text;
+
+namespace DotNet10PreviewLab
+{
+    internal class OrderedDictionaryTryAdd
+    {
+        public static void Execute()
+        {
+            //指定された文字列が何回出現したかをカウントする
+            var text = "apple banana apple cherry banana apple";
+            var orderedDictionary = new OrderedDictionary<string, int>();
+            foreach (var word in text.Split(' '))
+            {
+                IncrementValue(orderedDictionary, word);
+            }
+            foreach (var pair in orderedDictionary)
+            {
+                Console.WriteLine($"{pair.Key}: {pair.Value}");
+            }
+        }
+
+        private static void IncrementValue(OrderedDictionary<string, int> orderedDictionary, string key)
+        {
+            // Try to add a new key with value 1.
+            if (!orderedDictionary.TryAdd(key, 1, out int index))
+            {
+                // Key was present, so increment the existing value instead.
+                int value = orderedDictionary.GetAt(index).Value;
+                orderedDictionary.SetAt(index, value + 1);
+            }
+        }
+
+    }
+}
